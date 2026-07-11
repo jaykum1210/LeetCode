5e54e79e5ae8dd230c0a3f86645959366a9d2b4e
Jay Kumawat <jaykumawat1210@gmail.com>
2026-07-11 21:25:25 +0530

    Time: 1 ms (99.98%), Space: 46.5 MB (57.92%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,12 +1,18 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int[] arr = new int[26];
-        for(char ch :magazine.toLowerCase().toCharArray()){
-            arr[ch-'a']++;
+        if(magazine.length() < ransomNote.length()) return false;
+        int[] ransomNoteFrequency = new int[26];
+        int[] magazineFrequency = new int[26];
+        for(int i = 0; i < ransomNote.length(); i++){
+            ransomNoteFrequency[ransomNote.charAt(i) - 'a']++;
         }
-        for(char ch : ransomNote.toLowerCase().toCharArray()){
-            if(arr[ch-'a']==0) return false;
-            arr[ch - 'a']--;
+        for(int i = 0; i < magazine.length(); i++){
+            magazineFrequency[magazine.charAt(i) - 'a']++;
+        }
+        for(int i = 0; i < 26; i++) {
+            if(ransomNoteFrequency[i] > magazineFrequency[i]){
+                return false;
+            }
         }
         return true;
     }
