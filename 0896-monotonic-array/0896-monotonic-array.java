5596cdf8ad9bf9e6859d8d8cd19ae661be01ec53
Jay Kumawat <jaykumawat1210@gmail.com>
2026-07-15 21:20:58 +0530

    Time: 2 ms (66.4%), Space: 85.1 MB (61.16%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.java
+++ 0896-monotonic-array/0896-monotonic-array.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
+        if(nums.length==1){
+            return true;
+        }
         int decrease = 0;
         int increase = 0;
         for(int i = 0;i<nums.length-1;i++){
