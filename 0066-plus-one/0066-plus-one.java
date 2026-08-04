830aa55e48d5e08e8038cae5ecdcb4b0138c1f10
Jay Kumawat <jaykumawat1210@gmail.com>
2026-08-04 17:16:54 +0530

    Time: 0 ms (100%), Space: 43.6 MB (8.66%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,6 +1,5 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int carry = 0;
         for(int i = digits.length-1;i>=0;i--){
             if(digits[i]<9){
                 digits[i]++;
@@ -8,8 +7,8 @@ class Solution {
             }
             digits[i] = 0;
         }
-        int[] ans = new int[digits.length+1];
-        ans[0] = 1;
-        return ans;
+        int[] arr = new int[digits.length+1];
+        arr[0] = 1;
+        return arr;
     }
 }
