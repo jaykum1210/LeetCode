428a91a3c2ca6751020ef388efb23648d176ef72
Jay Kumawat <jaykumawat1210@gmail.com>
2026-08-05 19:34:00 +0530

    Time: 3 ms (78.31%), Space: 42.8 MB (86.17%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -6,21 +6,15 @@ class Solution {
         for(int i = 1;i<nums.length;i++){
             if(nums[i]==nums[i-1]+1) continue;
             else{
-                if(start==i-1){
-                    list.add(String.valueOf(nums[start]));
-                }
+                if(start==i-1) list.add(String.valueOf(nums[start]));
                 else{
                     list.add(nums[start] + "->" + nums[i-1]);
                 }
                 start = i;
             }
         }
-        if(start==nums.length-1){
-            list.add(String.valueOf(nums[start]));
-        }
-        else{
-            list.add(nums[start] + "->" + nums[nums.length-1]);
-        }
+        if(start==nums.length-1) list.add(String.valueOf(nums[start]));
+        else list.add(nums[start] + "->" + nums[nums.length-1]);
         return list;
     }
 }
