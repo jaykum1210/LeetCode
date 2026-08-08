9f8622ca50e295a8b6fc1b1f43d6b2387876afdc
Jay Kumawat <jaykumawat1210@gmail.com>
2026-08-08 20:13:18 +0530

    Time: 5 ms (81.23%), Space: 45.9 MB (72.66%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -4,11 +4,10 @@ class Solution {
         int ori = x;
         int rev = 0;
         while(x!=0){
-            int b = x%10;
-            if(rev>Integer.MAX_VALUE/10 || rev<Integer.MIN_VALUE/10) return false;
-            rev = rev*10 + b;
+            if(rev<Integer.MIN_VALUE/10 || rev>Integer.MAX_VALUE/10) return false;
+            rev = rev*10 + (x%10);
             x/=10;
         }
-        return rev==ori;
+        return ori==rev;
     }
 }
