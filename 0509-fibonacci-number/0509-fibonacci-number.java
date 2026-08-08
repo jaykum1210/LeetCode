950cd1305f38ca76dd7cbe33bb6e32f5d2fab5f5
Jay Kumawat <jaykumawat1210@gmail.com>
2026-08-08 21:18:40 +0530

    Time: 0 ms (100%), Space: 41.9 MB (71.01%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,7 +1,14 @@
 class Solution {
     public int fib(int n) {
         if(n==0) return 0;
-        else if(n==1) return 1;
-        return fib(n-1) + fib(n-2);
+        int first = 0;
+        int second = 1;
+        int sum = 1;
+        for(int i = 2;i<n;i++){
+            first = second;
+            second = sum;
+            sum = first+second;
+        }
+        return sum;
     }
 }
