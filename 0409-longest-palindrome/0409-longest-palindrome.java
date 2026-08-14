0ee2028682e9b936690ff17cf5c809a079159119
Jay Kumawat <jaykumawat1210@gmail.com>
2026-08-14 20:05:38 +0530

    Time: 7 ms (50.01%), Space: 43.5 MB (11.91%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        Set<Character> set = new HashSet<>();
+        int count = 0;
+        for(int i = 0;i<s.length();i++){
+            if(set.contains(s.charAt(i))){
+                count+=2;
+                set.remove(s.charAt(i));
+            }
+            else set.add(s.charAt(i));
+        }
+        if(!set.isEmpty()) count++;
+        return count;
+    }
+}
