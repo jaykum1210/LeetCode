eda75777944889ea6c5f97ec7cac104088abc6e5
Jay Kumawat <jaykumawat1210@gmail.com>
2026-08-16 18:15:00 +0530

    Time: 0 ms (100%), Space: 42 MB (90.25%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int hammingDistance(int x, int y) {
+        int num = x^y;
+        int count = 0;
+        while(num!=0){
+            if(num%2==1) count++;
+            num/=2;
+        }
+        return count;
+    }
+}
