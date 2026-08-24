b40b677c4c84b65f3f59b2c0ce8cf25201acad88
Jay Kumawat <jaykumawat1210@gmail.com>
2026-08-24 10:23:18 +0530

    Time: 34 ms (59.87%), Space: 60.3 MB (11.45%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,33 +1,28 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> OuterList = new ArrayList<>();
         Arrays.sort(nums);
-        List<List<Integer>> outer = new ArrayList<>();
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1])
-                continue;
-            int left = i + 1;
+        for(int i =  0; i < nums.length-2;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int left = i+1;
             int right = nums.length - 1;
-            while (left < right) {
+            while(left<right){
                 int sum = nums[i] + nums[left] + nums[right];
-                if (sum == 0) {
-                    List<Integer> inner = new ArrayList<>();
-                    inner.add(nums[i]);
-                    inner.add(nums[left]);
-                    inner.add(nums[right]);
-                    outer.add(inner);
+                if(sum==0){
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[left]);
+                    list.add(nums[right]);
+                    OuterList.add(list);
                     left++;
                     right--;
-                    while (left < right && nums[left] == nums[left - 1])
-                        left++;
-                    while (left < right && nums[right] == nums[right + 1])
-                        right--;
-                } else if (sum > 0) {
-                    right--;
-                } else {
-                    left++;
+                    while(left<right && nums[left]==nums[left-1]) left++;
+                    while(right>left && nums[right]==nums[right+1]) right--;
                 }
+                else if(sum<0) left++;
+                else right--;
             }
         }
-        return outer;
+        return OuterList;
     }
 }
