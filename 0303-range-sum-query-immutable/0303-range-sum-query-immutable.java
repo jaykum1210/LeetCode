d6be3c7b73e35745da538b33d18c7f0bf6fc4331
Jay Kumawat <jaykumawat1210@gmail.com>
2026-08-24 19:42:22 +0530

    Time: 8 ms (70.57%), Space: 47.9 MB (11.61%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -3,7 +3,7 @@ class NumArray {
     int[] arr;
 
     public NumArray(int[] nums) {
-        arr = new int[nums.length];
+        arr = nums;
         arr[0] = nums[0];
         for(int i = 1;i<nums.length;i++){
             arr[i] = nums[i] + arr[i-1];
