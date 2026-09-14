8c0ef260984e1cea10c0746f0522d57905c0f63d
Jay Kumawat <jaykumawat1210@gmail.com>
2026-09-14 09:38:14 +0530

    Time: 0 ms (100%), Space: 44.9 MB (15.76%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,41 +1,32 @@
 class Solution {
-    public static int nextMax(int[] nums, int start, int end, int pivot){
-
-    int max = Integer.MAX_VALUE;
-    int maxindex = 0;
-
-    for(int i = end; i >= start; i--){
-
-        if(nums[i] > pivot && nums[i] < max){
-
-            max = nums[i];
-            maxindex = i;
-        }
-    }
-
-    return maxindex;
-}
-    public static void reverse(int[] nums, int pivot, int end){
-        while (pivot<end){
-            int temp = nums[pivot];
-            nums[pivot] = nums[end];
+    public static void reverse(int[] nums, int start, int end){
+        while(start<end){
+            int temp = nums[start];
+            nums[start] = nums[end];
             nums[end] = temp;
-            pivot++;
             end--;
+            start++;
         }
     }
     public void nextPermutation(int[] nums) {
         int pivot = -1;
-        for (int i = nums.length-1;i>0;i--){
-            if (nums[i]>nums[i-1]){
+        for(int i = nums.length - 1;i>0;i--){
+            if(nums[i]>nums[i-1]){
                 pivot = i-1;
                 break;
             }
         }
-        if (pivot!=-1){
-            int max = nextMax(nums,pivot+1,nums.length-1, nums[pivot]);
-            int temp = nums[max];
-            nums[max] = nums[pivot];
+        if(pivot!=-1){
+            int min = Integer.MAX_VALUE;
+            int minindex = nums.length-1;
+            for(int i = nums.length-1;i>pivot;i--){
+                if(nums[i]>nums[pivot] && nums[i]<min){
+                    min = nums[i];
+                    minindex = i;
+                }
+            }
+            int temp = nums[minindex];
+            nums[minindex] = nums[pivot];
             nums[pivot] = temp;
         }
         reverse(nums,pivot+1,nums.length-1);
