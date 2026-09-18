16ed7edb825c235b1f45fde2c26af7a6d34b9a4d
Jay Kumawat <jaykumawat1210@gmail.com>
2026-09-18 20:04:24 +0530

    Time: 1 ms (99.96%), Space: 42.9 MB (11.6%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -2,9 +2,9 @@ class Solution {
     public int reverse(int x) {
         long rev = 0;
         while(x!=0){
-            int num = x%10;
+            int b = x%10;
             if(rev>Integer.MAX_VALUE/10 || rev<Integer.MIN_VALUE/10) return 0;
-            rev = rev*10 + num;
+            rev = rev*10 + b;
             x/=10;
         }
         return (int)rev;
